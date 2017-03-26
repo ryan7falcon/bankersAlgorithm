2aecdc751cb92f57fb5136aadde0606d91ec8abf
Ryan Falcon <ryan7falcon@gmail.com>
2017-03-25 20:12:55 -0400

    implemented the algorithm

--- script.js
+++ script.js
@@ -6,14 +6,78 @@
 // License: MIT
 // Date: March 2017
 //
+
+
+// -----------------------------------------------------------------------------------------
+// Helper functions
+// -----------------------------------------------------------------------------------------
+
+// sum two arrays
+function sumArrays(a1, a2) {
+  return a1.map((el, i) => el + a2[i])
+}
+
+// subtract two arrays
+function subtractArrays(a1, a2) {
+  return a1.map((el, i) => el - a2[i])
+}
+
+
+// function scalarMultiplyArray(a, n) {
+//   return a.map(el => el * n)
+// }
+
+// check if each element of the array is greater or equal the respective element in another array
+function allElementsAreGreaterOrEqual(a1, a2) {
+  return a1.every((el, i) => el >= a2[i])
+}
+
+// Augmenting Array.prototype for array comparison is taken from https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+// Warn if overriding existing method
+if (Array.prototype.equals) {
+  console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.")
+}
+
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+  if (!array) {
+    return false
+  }
+
+  // compare lengths - can save a lot of time
+  if (this.length !== array.length) {
+    return false
+  }
+
+  for (let i = 0, l = this.length; i < l; i++) {
+    // Check if we have nested arrays
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      // recurse into the nested arrays
+      if (!this[i].equals(array[i])) {
+        return false
+      }
+    } else if (this[i] !== array[i]) {
+      // Warning - two different object instances will never be equal: {x:20} != {x:20}
+      return false
+    }
+  }
+  return true
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, 'equals', { enumerable: false })
+
 // -----------------------------------------------------------------------------------------
 // DATA
 // -----------------------------------------------------------------------------------------
-let bankersApp = {
-  numProcesses: 5,
+// number of processes
+const n = 5
+
+const appData = {
+  numProcesses: n,
   processPrefix: 'P',
   resources: ['A', 'B', 'C'],
-  available: [10, 5, 7],
+  available: [3, 3, 2],
   max: [
     [7, 5, 3],
     [3, 2, 2],
@@ -26,11 +90,174 @@ let bankersApp = {
     [3, 0, 2],
     [2, 1, 1],
     [0, 0, 2]],
-  request: [2, 4, 4],
-  sequence: [1, 2, 3],
-  status: 'Success!',
+  finished: (new Array(n)).fill(false),
+  request: [], // [ A, B, C]
+  processId: 0,
+  sequence: [],
+  status: '',
   error: '',
 }
+
+// -----------------------------------------------------------------------------------------
+// Functions for mutating the app state (Banker's Algoritm is in this section)
+// -----------------------------------------------------------------------------------------
+
+const appPrototype = {
+  // calculate needs array
+  needs() {
+    return this.max.map((m, i) => subtractArrays(m, this.allocation[i]))
+  },
+
+  // find safe sequence
+  findSafeSequence() {
+    // loop through all needs, execute whats possiible, free resources and move to next itearation
+    // if looped though all twice without deallocating memory, its a deadlock
+
+    // safe sequence will be stored here
+    let result = []
+    // create a copy of the state
+    const copy = this.copy()
+    // a flag to keep track of whether the iteration deallocated some memory
+    let retry = false
+
+    // try requesting needed memory
+    function tryNeed(need, i) {
+      // if the process is not finished
+      if (!copy.finished[i]) {
+        // check if it can be finished now by making a request that would satisfy the need
+        copy.request = need
+        copy.processId = i
+        // checking available resources
+        if (allElementsAreGreaterOrEqual(copy.available, need)) {
+          // if there are available resource, grant the request and deallocate memory
+          copy.allocate()
+          // add current process to the safe sequence
+          result.push(i)
+          // allow next iteration
+          retry = true
+        }
+      }
+    }
+
+    // iterate while each iteration deallocates memory
+    do {
+      retry = false
+      copy.needs().forEach(tryNeed)
+    } while (retry)
+
+    // check if sequence contains all remaining processes
+    if (result.length !== this.getNumberOfRemainingProcesses()) {
+      result = []
+    }
+
+    return result
+  },
+
+  getNumberOfRemainingProcesses() {
+    return this.finished.filter(x => x === false).length
+  },
+
+  // allocate memory for the process based on request, free the memory if the need is met
+  allocate() {
+    // allocate memory as per request
+    this.allocation[this.processId] = sumArrays(this.allocation[this.processId], this.request)
+
+    // check if allocation == max, then free the resources
+    if (this.allocation[this.processId].equals(this.max[this.processId])) {
+      // increase available resources
+      this.available = sumArrays(this.available, this.allocation[this.processId])
+
+      // set a fleag for the finished process
+      this.finished[this.processId] = true
+
+      // set allocation for the finished process to 0
+      this.allocation[this.processId].fill(0)
+
+      // set max for the finished process to 0
+      this.max[this.processId].fill(0)
+    }
+
+    // decrease available resources
+    this.available = subtractArrays(this.available, this.request)
+  },
+
+  // send request and check for safe sequence
+  sendRequest() {
+    const successStr = 'Success!'
+    const deniedStr = 'Request denied.'
+
+    if (!allElementsAreGreaterOrEqual(this.available, this.request)) {
+      this.error = 'The request exceeds available resources'
+      this.status = deniedStr
+      return
+    }
+
+    if (!allElementsAreGreaterOrEqual(this.needs()[this.processId], this.request)) {
+      this.error = 'The request exceeds the need (max - allocation)'
+      this.status = deniedStr
+      return
+    }
+
+    // check if request -> deadlock
+
+    // create a copy of the state
+    const test = this.copy()
+    // satisfy the request in the copy
+    test.allocate()
+    // find safe sequence for the copy
+    const seq = test.findSafeSequence()
+
+
+    // if there is no safe sequence, record error, else satidfy the request in the original
+    if (seq.length === 0 && test.getNumberOfRemainingProcesses() > 0) {
+      this.error = 'The request is not safe and might lead to a deadlock'
+      this.status = deniedStr
+    } else {
+      if (seq.length === 0) {
+        this.error = 'All processes have been completed'
+      } else {
+        this.error = ''
+      }
+      this.status = successStr
+      // remember safe sequence
+      this.sequence = seq
+      // satisfy the request for the original
+      this.allocate()
+    }
+  },
+
+  updateSafeSequence() {
+    const seq = this.findSafeSequence()
+
+    // if there is no safe sequence, record error, else satidfy the request in the original
+    if (seq.length === 0) {
+      if (this.getNumberOfRemainingProcesses() > 0) {
+        this.error = 'The system is in deadlock'
+      } else {
+        this.error = 'All processes have been completed'
+      }
+    } else {
+      this.error = ''
+      // remember safe sequence
+      this.sequence = seq
+    }
+  },
+
+  // create a deep copy of the object's data while keeping the methods in the new object's prototype
+  copy() {
+    return Object.assign(Object.create(this), JSON.parse(JSON.stringify(this)))
+  },
+}
+
+
+// app factory
+const bankersAppFactory = function factory(data) {
+  return Object.assign(Object.create(appPrototype), data)
+}
+
+// create an instance of the app
+const bankersApp = bankersAppFactory(appData)
+
 
 // -----------------------------------------------------------------------------------------
 // templates (pure functions)
@@ -41,7 +268,7 @@ function resourceWrapper(heading, valuesArr, resourcesArr) {
 
   for (let i = 0; i < resourcesArr.length; i++) {
     str += `<div class="resource">
-      <span>${resourcesArr[i]}</span><span>${valuesArr[i]}</span>
+      <span class="resourceName">${resourcesArr[i]}</span><span>${valuesArr[i]}</span>
     </div>`
   }
 
@@ -55,13 +282,11 @@ function resourceWrapper(heading, valuesArr, resourcesArr) {
   `
 }
 
-// returns html for log rows
-function logRow(processId, processPrefix, status, sequence, error,
-  requestArr, allocationArr, availableArr, resourcesArr) {
+// html for displaying safe sequence
+function safeSequence(sequence, processPrefix, error) {
   let seqStr = ''
 
   if (error === '') {
-    seqStr = 'Safe sequence: '
     sequence.forEach((id, i) => {
       seqStr += processPrefix + id
       if (i < sequence.length - 1) {
@@ -69,18 +294,30 @@ function logRow(processId, processPrefix, status, sequence, error,
       }
     })
   }
+  return seqStr
+}
+
+// returns html for log rows
+function logRow(processId, processPrefix, status, sequence, error,
+  requestArr, allocationArr, availableArr, resourcesArr) {
+  let seqStr = safeSequence(sequence, processPrefix, error)
+  if (seqStr !== '') {
+    seqStr = `Safe sequence: ${seqStr}`
+  }
+
+  const processName = `<span class="processName">${processPrefix}${processId}</span>`
 
   return `
     <div class="logRow panel">
       <div class="status">
-        <span class="processName">${processPrefix}${processId}</span>
+        ${processName}
         <span class="success">${status}</span>
         <span class="sequence">${seqStr}</span>
         <span class="error">${error}</span>
       </div>
       <div class="logDetails">
-        ${resourceWrapper('Request', requestArr, resourcesArr)}
-        ${resourceWrapper('New Allocation', allocationArr, resourcesArr)}
+        <div class="resourceWithProcessName">${processName}${resourceWrapper('Request', requestArr, resourcesArr)}</div>
+        <div class="resourceWithProcessName">${processName}${resourceWrapper('New Allocation', allocationArr, resourcesArr)}</div>
         ${resourceWrapper('New Available', availableArr, resourcesArr)}
       </div>
     </div>
@@ -88,20 +325,26 @@ function logRow(processId, processPrefix, status, sequence, error,
 }
 
 // returns html for one row for the table with allocation and max
-function processRow(processId, processName, allocationArr, maxArr, resourcesArr) {
+function processRow(processId, processName, allocationArr,
+  needArr, maxArr, resourcesArr, finished) {
   let allocationStr = ''
+  let needStr = ''
   let maxStr = ''
 
   for (let i = 0; i < resourcesArr.length; i++) {
     allocationStr += `<span>${allocationArr[i]}</span>`
+    needStr += `<span>${needArr[i]}</span>`
     maxStr += `<span>${maxArr[i]}</span>`
   }
 
   return `
-    <tr class="processRow panel" data-process="${processId}">
+    <tr class="processRow panel ${finished[processId] ? 'finished' : ''}" data-process="${processId}">
       <td class="processCell">${processName}</td>
       <td><div>
         ${allocationStr}
+      </div></td>
+      <td><div>
+        ${needStr}
       </div></td>
       <td><div>
         ${maxStr}
@@ -116,17 +359,18 @@ function availableResources(availableArr, resourcesArr) {
 
   for (let i = 0; i < resourcesArr.length; i++) {
     availableStr += `<div class="resource">
-      <span>${resourcesArr[i]}</span><span>${availableArr[i]}</span>
+      <span class="resourceName">${resourcesArr[i]}</span><span>${availableArr[i]}</span>
     </div>`
   }
   return availableStr
 }
 
 // returns the inside of the table
-function table(numProcesses, processPrefix, allocationArr, maxArr, resourcesArr) {
+function table(numProcesses, processPrefix,
+  allocationArr, needArr, maxArr, resourcesArr, finished) {
   let tableRows = ''
   for (let i = 0; i < numProcesses; i++) {
-    tableRows += processRow(i, `${processPrefix}${i}`, allocationArr[i], maxArr[i], resourcesArr)
+    tableRows += processRow(i, `${processPrefix}${i}`, allocationArr[i], needArr[i], maxArr[i], resourcesArr, finished)
   }
 
   let resourcesStr = ''
@@ -142,6 +386,9 @@ function table(numProcesses, processPrefix, allocationArr, maxArr, resourcesArr)
         <div class="header headerTable">Allocation</div>
       </td>
       <td>
+        <div class="header headerTable">Need</div>
+      </td>
+       <td>
         <div class="header headerTable">Max</div>
       </td>
     </tr>
@@ -151,6 +398,9 @@ function table(numProcesses, processPrefix, allocationArr, maxArr, resourcesArr)
         <div>${resourcesStr}</div>
       </td>
       <td>
+        <div>${resourcesStr}</div>
+      </td>
+        <td>
         <div>${resourcesStr}</div>
       </td>
     </tr>
@@ -178,8 +428,10 @@ function renderAvailable(availableArr, resourcesArr) {
 }
 
 // render table rows with allocation and max
-function renderTable(numProcesses, processPrefix, allocationArr, maxArr, resourcesArr) {
-  document.querySelector('#table').innerHTML = table(numProcesses, processPrefix, allocationArr, maxArr, resourcesArr)
+function renderTable(numProcesses, processPrefix, allocationArr,
+  needArr, maxArr, resourcesArr, finished) {
+  document.querySelector('#table').innerHTML = table(numProcesses, processPrefix,
+   allocationArr, needArr, maxArr, resourcesArr, finished)
 }
 
 //
@@ -196,10 +448,27 @@ function renderInputs(resourcesArr, numProcesses) {
   }
 }
 
+function renderSafeSequence(sequence, processPrefix, error) {
+  const str = safeSequence(sequence, processPrefix, error)
+  document.getElementById('safeSequence').innerHTML = str === '' ? error : str
+}
+
 // render current state - available and table
 function render(app) {
   renderAvailable(app.available, app.resources)
-  renderTable(app.numProcesses, app.processPrefix, app.allocation, app.max, app.resources)
+  renderTable(app.numProcesses, app.processPrefix,
+    app.allocation, app.needs(), app.max, app.resources, app.finished)
+
+  app.updateSafeSequence()
+  renderSafeSequence(app.sequence, app.processPrefix, app.error)
+
+  // remove finished process from select
+  const processSelect = document.getElementById('process')
+  for (let i = 0; i < processSelect.length; i++) {
+    if (app.finished[processSelect[i].value]) {
+      processSelect.remove(i)
+    }
+  }
 }
 
 // render log entries
@@ -210,46 +479,33 @@ function renderLogEntry(processId, processPrefix, status, sequence, error,
   document.querySelector('.logContainer').insertAdjacentHTML('afterbegin', logRowStr)
 }
 
-// -----------------------------------------------------------------------------------------
-// functions for mutating the app state
-// -----------------------------------------------------------------------------------------
-// returns the new app state based on request and render results
-function sendRequest(app) {
-  // new app state will be saved here
-  const newApp = app
-
-  // get requested process ID
-  const processId = document.querySelector('#process>option:checked').value
-  console.log(processId)
-
-  // get requested resources from input
-  app.resources.forEach((resource, i) => {
-    newApp.request[i] = parseInt(document.querySelector(`#${resource}`).value, 10) || 0
-  })
-
-  // TODO: update app data
-  // check if request < max - allocated
-  // check if request < available
-  // check if request -> deadlock
-  // print safe sequence
-
-  // render results
-  renderLogEntry(processId, newApp.processPrefix, newApp.status, newApp.sequence, newApp.error,
-    newApp.request, newApp.allocation[processId], newApp.available, newApp.resources)
-  render(newApp)
-
-  return newApp
-}
 
 // -----------------------------------------------------------------------------------------
 // run the app
 // -----------------------------------------------------------------------------------------
 
-// handle button click - modify the state and render results
-document.querySelector('#sendRequest').addEventListener('click', () => {
-  bankersApp = sendRequest(bankersApp)
-})
-
 // initial app render
 renderInputs(bankersApp.resources, bankersApp.numProcesses)
-render(bankersApp)
+render(bankersApp) // available and table
+// returns the new app state based on request and render results
+function handleClick() {
+  // get requested process ID from select
+  bankersApp.processId = document.querySelector('#process>option:checked').value
+
+  // get requested resources from input
+  bankersApp.resources.forEach((resource, i) => {
+    bankersApp.request[i] = Math.abs(parseInt(document.querySelector(`#${resource}`).value, 10)) || 0
+  })
+
+  // process the request
+  bankersApp.sendRequest()
+
+  // render results
+  renderLogEntry(bankersApp.processId, bankersApp.processPrefix, bankersApp.status,
+   bankersApp.sequence, bankersApp.error, bankersApp.request,
+   bankersApp.allocation[bankersApp.processId], bankersApp.available, bankersApp.resources)
+  render(bankersApp) // available and table
+}
+
+// handle button click - modify the state and render results
+document.querySelector('#sendRequest').addEventListener('click', handleClick)
